0f63539da7e945b53a79c35655ea49fc87bf27c6
Rajendra kumar <rajendraku0000@gmail.com>
2023-09-05 20:32:24 +0530

    update

--- Hackar rank important question.sql
+++ Hackar rank important question.sql
@@ -24,3 +24,24 @@ FROM STATION;
 -- 2 nd method 
 -- SELECT CITY, length(CITY) as len FROM STATION ORDER BY len, CITY LIMIT 1;
 -- SELECT CITY, length(CITY) as len FROM STATION ORDER BY len DESC, CITY LIMIT 1;
+
+
+
+-- 3. Consider P1(a,b) and P2(c,d) to be two points on a 2D plane. 
+/* a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+Query the Manhattan Distance between points P1 and P2 and round it to a scale of 4 decimal places. */
+
+
+SET @a := (SELECT MIN(LAT_N) FROM STATION);
+SET @b := (SELECT MIN(LONG_W) FROM STATION);
+SET @c := (SELECT MAX(LAT_N) FROM STATION);
+SET @d := (SELECT MAX(LONG_W) FROM STATION);
+
+SELECT ROUND(ABS(@a - @c) + ABS(@b - @d), 4);
+
+
+
+
